af98f5363a4cef79c415583fb696ee39b1e8f6d7
johnscott <johnoscott@gmail.com>
2018-04-01 17:41:29 +1000

    add pizza

--- api.py
+++ api.py
@@ -11,7 +11,7 @@ api = Api(app)
 class Product(Resource):
     def get(self):
         return {
-            'products': ['Ice cream', 'Chocolate', 'Fruit', 'Eggs', 'Bread']
+            'products': ['Ice cream', 'Chocolate', 'Fruit', 'Eggs', 'Bread', 'Pizza']
         }
 
 # Create routes
